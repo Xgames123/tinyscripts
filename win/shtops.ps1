39ae06ab38287cd4bd4f6246ab8601691bf6cd96
ldev <ldev@ldev.eu.org>
2023-08-10 14:50:09 +0200

    improved alias gen in shtops

--- win/shtops.ps1
+++ win/shtops.ps1
@@ -6,7 +6,7 @@ $shcode = $args[0]
 
 # aliases
 # NOTE: aliases need to be converted to functions because powershell doesn't support args in aliases
-$shcode = $($shcode -ireplace "^alias ([a-z]*)=[`"']?([^'`"\n]*)[`"']?", 'function $1(){$2}')
+$shcode = $($shcode -ireplace "^alias ([a-z]*)=[`"']?([^'`"\n]*)[`"']?", "<# shtops:ALIAS #>function `$1(){`"`$2 `$(`$args -join ' ')`" | Invoke-Expression}")
 
 
 echo $shcode
